2bd70d23aa7eeee9b94163f4609ccb97e8dd1b47
robertandrews <mail@robertandrews.co.uk>
2023-04-28 19:26:37 +0100

    Changed find-api-url pattern, and more

--- content-grabber.php
+++ content-grabber.php
@@ -8,6 +8,13 @@
  * Author URI: http://www.contexthq.com
  */
 
+ini_set('max_execution_time', 3000); // 300 seconds = 5 minutes
+set_time_limit(3000);
+// override timeout
+add_filter('http_request_timeout', function () {
+    return 30000; // 300 seconds = 5 minutes
+});
+
 require __DIR__ . '/vendor/autoload.php';
 use Graby\Graby;
 
@@ -35,42 +42,36 @@ function content_grabber_admin_page()
      * @return void
      */
 
+    // If this was a -submitting form, process it
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        // Get the content of the 'content_url' input field
         $content_url = esc_url_raw($_POST['content_url']);
-        // $content_response = wp_remote_get($content_url);
 
-        // TODO: Not this
-        /*
-        if (!is_wp_error($api_response)) {
-        $api_response_body = json_decode(wp_remote_retrieve_body($api_response), true);
-        $posts_data = content_grabber_handle_api_response($api_response_body);
-        }
-         */
+        // If no error
         if (!is_wp_error($content_url)) {
-            // content_grabber_director($content_url);
 
-            if (strstr($content_url, 'wp-json')) {
-                // WP-JSON URL
-                echo "That's a WP-JSON URL";
-                // grab_from_json($content_url);
-                // TODO: Fetch the JSON to $post_data
-            } elseif (strstr($content_url, 'feed')) {
-                // RSS URL
-                echo "That's an RSS feed URL";
-                // grab_from_rss($content_url);
+            // Get content from either WordPress API discovery or Graby parsing
+            $post_package = get_article_content($content_url);
+
+            // Add new post
+            if ($post_package) {
+                echo "we have a post package, lets add a post...\n";
+                $created_post_id = content_grabber_add_post($post_package, $content_url);
+                /*
+            if ($created_post_id) {
+            return $created_post_id;
+            wp_redirect(admin_url('post.php?post=' . $created_post_id . '&action=edit'));
+
+            }
+             */
             } else {
-                // Normal URL
-                // echo "normal URL";
-                // $created_post_id = grab_from_web($content_url);
-                if ($created_post_id) {
-                    wp_redirect(admin_url('post.php?post=' . $created_post_id . '&action=edit'));
-                }
+                // echo "Could not add new post.";
             }
 
         }
 
     }
-
     ?>
 <div class="wrap">
     <h2>Content Grabber</h2>
@@ -85,89 +86,23 @@ function content_grabber_admin_page()
         </table>
         <?php submit_button('Fetch Post/s');?>
     </form>
-    <?php
-// TODO: Not this
-
-    if (isset($posts_data) && is_array($posts_data)) {
-        echo '<h2>Available Posts</h2>';
-        echo '<table class="wp-list-table widefat fixed striped posts">';
-        echo '<thead>';
-        echo '<tr>';
-        echo '<th scope="col" id="cb" class="manage-column column-cb check-column"><input id="cb-select-all-1" type="checkbox"></th>';
-        echo '<th scope="col" id="title" class="manage-column column-title column-primary">Title</th>';
-        echo '</tr>';
-        echo '</thead>';
-
-        echo '<tbody id="the-list">';
-        foreach ($posts_data as $post_data) {
-            echo '<tr>';
-            echo '<th scope="row" class="check-column"><input type="checkbox"></th>';
-            echo '<td class="title column-title has-row-actions column-primary"><strong><a href="' . $post_data['link'] . '">' . $post_data['title']['rendered'] . '</a></strong></td>';
-            echo '</tr>';
-        }
-        echo '</tbody>';
-
-        echo '</table>';
-        echo '<button type="button" class="button button-primary">Import Selected Posts</button>';
-    }
-    ?>
-
 </div>
 <?php
 }
 
-function content_grabber_director($content_url)
+function get_article_content($content_url)
 {
-    /**
-     * Grab article from URL
-     * @return void
-     */
-
-    if (strstr($content_url, 'wp-json')) {
-        // WP-JSON URL
-        grab_from_json($content_url);
-    } elseif (strstr($content_url, 'feed')) {
-        // RSS URL
-        echo "That's an RSS feed URL";
-        // grab_from_rss($content_url);
-    } else {
-        // Normal URL
-        echo "normal URL";
-        // $created_post_id = grab_from_web($content_url);
-        if ($created_post_id) {
-            wp_redirect(admin_url('post.php?post=' . $created_post_id . '&action=edit'));
-        }
-    }
-}
-
-function grab_from_web($content_url)
-{
-
-    // echo $content_url;
 
     // 🔻 Retrieve the page source code
     $response = wp_remote_get($content_url);
-
-    // file_put_contents('response.txt', print_r($response, true));
-
-    /*
-    if ($response['body']) {
-    $response = $response['body'];
-    }
-     */
     $body = wp_remote_retrieve_body($response);
-    // file_put_contents('body.txt', print_r($response, true));
 
-    /*
-    echo "print body";
-    print_r($body);
-     */
+    // Two options for getting the post content:
 
-    // 👉🏻 WordPress? Use any discovered /posts/ URL, eg. /wp-json/wp/v2/posts/2525617
+    // 1. WordPress - Look for WordPress API endpoint for individual post, eg. /wp-json/wp/v2/posts/2525617
     if ($wp_post_api_url = wp_find_post_api_url($body)) {
 
-        // print_r("WordPress discovered.");
-        // echo $wp_post_api_url;
+        print_r("WordPress discovered, found post endpoint " . $wp_post_api_url . "\n");
 
         $path = parse_url($wp_post_api_url, PHP_URL_PATH);
         $segments = explode('/', $path);
@@ -177,54 +112,29 @@ function grab_from_web($content_url)
         $post_id = $segments[5];
 
         // Call /posts/ for a single post
-        $posts_json = wp_call_api_posts($post_type, $post_id, $content_url);
-        // print_r($posts_json);
+        $post_json = wp_do_api_posts($post_type, $post_id, $content_url);
 
         // Package-up /posts/ response to set as new WP post
-        $post_package = package_wp_post($posts_json);
+        $post_package = package_wp_post($post_json);
+
+        // print_r($post_json);
+
+        return $post_package;
 
     } else {
-        // 👉🏻 Not WordPress: Use Graby to parse the page
+        // 2. Not WordPress: Use Graby to parse the page
 
-        // echo "Graby pathway";
+        echo "Could not find WordPress, using Graby.\n";
 
+        // Use Graby to parse the article
         $graby = new Graby();
-
         $result = $graby->fetchContent($content_url);
 
+        // Package-up Graby response to set as new WP post
         $post_package = package_graby_post($result);
 
-    }
+        return $post_package;
 
-    // Add new post
-    if ($post_package) {
-        echo "we have a post package, lets add a post...\n";
-        $created_post_id = content_grabber_add_post($post_package, $content_url);
-        // TODO: If something else re JSON
-        if ($created_post_id) {
-            return $created_post_id;
-        }
-    } else {
-        // echo "Could not add new post.";
-    }
-
-}
-
-function grab_from_json()
-{
-    $url = 'https://example.com/wp-json/wp/v2/posts';
-
-    $response = wp_remote_get($url);
-
-    if (is_wp_error($response)) {
-        echo 'Error fetching posts: ' . $response->get_error_message();
-    } else {
-        $body = wp_remote_retrieve_body($response);
-        $posts = json_decode($body);
-
-        foreach ($posts as $post) {
-            echo '<h2>' . $post->title->rendered . '</h2>';
-        }
     }
 
 }
@@ -233,7 +143,7 @@ function wp_find_post_api_url($body)
 {
     // 1. Some post markup contains the actual endpoint
     // eg. /wp-json/wp/v2/posts/2525617 & /wp-json/wp/v2/news/1337292
-    if (preg_match('/\/wp-json\/wp\/v2\/(?!.*media|categories|tags).*\/\d+/', $body, $matches)) {
+    if (preg_match('/\/wp-json\/wp\/v2\/(?!.*media|categories|tags).*\/\d+(?=")/', $body, $matches)) {
         $wp_post_api_url = $matches[0];
         // echo 'The post endpoint is: ' . $wp_post_api_url;
         return $wp_post_api_url;
@@ -263,7 +173,7 @@ function wp_find_post_id_body($body)
 
 }
 
-function wp_call_api_posts($post_type, $post_id, $content_url)
+function wp_do_api_posts($post_type, $post_id, $content_url)
 {
 
     // Construct API URL
@@ -314,6 +224,30 @@ function package_wp_post($posts_json)
     // TODO: If $posts_json->_embedded->{'wp:featuredmedia'}[0]->data->status = 401 (forbidden),,
     // run a /media/ call instead to get the image
     $post_data['feat_img_url'] = isset($posts_json->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->full->source_url) ? $posts_json->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->full->source_url : '';
+
+    // Set all the WordPress $post_json wp:term terms matching taxonomy "topic" to the $post_data['topics'] array
+    if (isset($posts_json->_embedded->{'wp:term'})) {
+        $post_data['topics'] = array();
+        foreach ($posts_json->_embedded->{'wp:term'} as $term) {
+            foreach ($term as $topic) {
+                if ($topic->taxonomy == 'topic') {
+                    $post_data['topics'][] = $topic->name;
+                }
+            }
+        }
+    }
+
+    // Set all the WordPress $post_json wp:term terms matching taxonomy "topic" to the $post_data['topics'] array
+    if (isset($posts_json->_embedded->{'wp:term'})) {
+        $post_data['companies'] = array();
+        foreach ($posts_json->_embedded->{'wp:term'} as $term) {
+            foreach ($term as $company) {
+                if ($company->taxonomy == 'company') {
+                    $post_data['companies'][] = $company->name;
+                }
+            }
+        }
+    }
 
     return $post_data;
 
@@ -369,8 +303,6 @@ function package_graby_post($result)
 function content_grabber_add_post($post_data, $content_url)
 {
 
-    print_r("Inside function...");
-
     // Get author name
     if ($post_data['author_name'] != '') {
         $assigned_author_id = assign_author($post_data['author_name']);
@@ -393,19 +325,33 @@ function content_grabber_add_post($post_data, $content_url)
     // Add post extras
     if ($created_post_id) {
 
+        // Set source publication
+        wp_set_post_source($created_post_id, $content_url);
+        do_my_log("wp_set_post_source");
+
+        // Add article source URL
+        update_post_meta($created_post_id, 'source_url', $content_url);
+        do_my_log("updated source_url");
+
+        // Add date obtained
+        update_post_meta($created_post_id, 'date_obtained', date('Y-m-d H:i:s'));
+        do_my_log("updated date");
+
+        wp_set_post_topics($created_post_id, $post_data);
+        do_my_log("added topics");
+
+        wp_set_post_companies($created_post_id, $post_data);
+        do_my_log("added companies");
+
         // Add featured image
         if ($post_data['feat_img_url']) {
             wp_add_featured_media($post_data['feat_img_url'], $created_post_id);
         }
 
-        // Set source publication
-        wp_set_post_source($created_post_id, $content_url);
-
-        // Add article source URL
-        update_post_meta($created_post_id, 'source_url', $content_url);
-
-        // Add date obtained
-        update_post_meta($created_post_id, 'date_obtained', date('Y-m-d H:i:s'));
+        $post = array(
+            'ID' => $created_post_id,
+        );
+        wp_update_post($post);
 
         return $created_post_id;
 
@@ -468,6 +414,7 @@ function wp_add_featured_media($file_url, $created_post_id)
     // Download file and add it to the post
     $upload_dir = wp_upload_dir();
     $image_data = wp_remote_get($file_url);
+    do_my_log("got image_data");
     $filename = basename($file_url);
     if (wp_mkdir_p($upload_dir['path'])) {
         $file = $upload_dir['path'] . '/' . $filename;
@@ -475,36 +422,103 @@ function wp_add_featured_media($file_url, $created_post_id)
         $file = $upload_dir['basedir'] . '/' . $filename;
     }
     file_put_contents($file, $image_data['body']);
+    do_my_log("put image_data");
     $wp_filetype = wp_check_filetype($filename, null);
+    do_my_log("checked file type");
     $attachment = array(
-        'post_mime_type' => $wp_filetype['type'],
+        'post_mime_type' => $wp_filetype['type'], // Is this really necessary?
         'post_title' => sanitize_file_name($filename),
         'post_content' => '',
         'post_status' => 'inherit',
     );
     $attach_id = wp_insert_attachment($attachment, $file, $created_post_id);
+    do_my_log("inserted attachment " . $attach_id);
     require_once ABSPATH . 'wp-admin/includes/image.php';
     $attach_data = wp_generate_attachment_metadata($attach_id, $file);
+    do_my_log("generated attachment metadata");
     $res1 = wp_update_attachment_metadata($attach_id, $attach_data);
+    do_my_log("updated attachment metadata");
     $res2 = set_post_thumbnail($created_post_id, $attach_id);
+    do_my_log("set post thumbnail");
+    // echo $res2;
+
     // re-save the post to update the post thumbnail
     $post = array(
         'ID' => $created_post_id,
         // 'post_content' => $post_json->content->rendered,
     );
-    wp_update_post($post);
+    // wp_update_post($post);
 
 }
 
 function wp_set_post_source($created_post_id, $content_url)
 {
 
+    // Set the new post's source to a slug matching the site's hose
+
     $host = parse_url($content_url, PHP_URL_HOST); // Get host name
     $pieces = explode(".", $host); // Split host name by dot
     $source_slug = ($pieces[0] === 'www') ? $pieces[1] : $pieces[0]; // Get the first portion of the host name, or the second if the first is "www"
-
     // echo $source; // Output: "theguardian"
 
     wp_set_object_terms($created_post_id, $source_slug, 'source', true);
+
+    if ($source_slug == 'themap') {
+        // If the source is "themap", set the source to "map"
+        if (term_exists('news-uk', 'client')) {
+            wp_set_object_terms($created_post_id, 'news-uk', 'client', true);
+        }
+    }
+
+}
+
+function wp_set_post_topics($created_post_id, $post_data)
+{
+
+    // If there are any topics and the taxonomy "topic" exists, add them
+    if (isset($post_data['topics']) && taxonomy_exists('topic')) {
+        foreach ($post_data['topics'] as $topic) {
+            // If the term exists, set it by ID
+            if (term_exists($topic, 'topic')) {
+                $term = get_term_by('name', $topic, 'topic');
+                echo "term exists\n";
+                wp_set_post_terms($created_post_id, $term->term_id, 'topic', true);
+                echo "term set\n";
+            } else {
+                // If the term doesn't exist, create it and set it by ID
+                $term = wp_insert_term($topic, 'topic');
+                echo "term created\n";
+                wp_set_post_terms($created_post_id, $term['term_id'], 'topic', true);
+                echo "term set\n";
+            }
+        }
+    }
+
+}
+
+function wp_set_post_companies($created_post_id, $post_data)
+{
+
+    // If there are any companies and the taxonomy "company" exists, add them
+    if (isset($post_data['companies']) && taxonomy_exists('company')) {
+        echo "companies exist\n";
+        foreach ($post_data['companies'] as $company) {
+            // If the term exists, set it by ID
+            if (term_exists($company, 'company')) {
+                $term = get_term_by('name', $company, 'company');
+                echo "term exists\n";
+                wp_set_post_terms($created_post_id, $term->term_id, 'company', true);
+                echo "term set\n";
+            } else {
+                // If the term doesn't exist, create it and set it by ID
+                $term = wp_insert_term($company, 'company');
+                echo "term created\n";
+                wp_set_post_terms($created_post_id, $term['term_id'], 'company', true);
+                echo "term set\n";
+            }
+        }
+    } else {
+        echo "companies don't exist\n";
+    }
 
 }
