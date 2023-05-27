f76c89ab5cb6dc6b14a77b3f10505423430dc978
robertandrews <mail@robertandrews.co.uk>
2023-05-27 21:57:15 +0100

    Clean up

--- script-test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-grab_from_web('https://themap.news/post/alcohol-brands-are-going-down-funnel-up-market-and-off-plan/');
-
-// wp_get_post_id_from_url('https://themap.news/post/371-false-consensus/');
